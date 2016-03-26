8e49ea4e288e1780af1ad5ebc09fbf07b707decc
Mario Celi <mcelicalderon@gmail.com>
2016-03-26 12:21:22 -0500

    Add bootstrap style

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'pry'
 
 gem 'jquery-rails'
+gem 'select2-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,8 @@ Rails.application.routes.draw do
       resources :species
     end
   end
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
